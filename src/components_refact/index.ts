a36c54874ef00f65c439e77a99d8f43499f492fd
George051191 <trubacheff_91@mail.ru>
2022-04-25 16:44:20 +0300

    some fixes

--- src/components_refact/index.ts
+++ src/components_refact/index.ts
@@ -3,6 +3,9 @@ import { EditProfileSettings } from "./ui_lib/edit-profile-settings";
 import { FollowUserButton } from "./ui_lib/follow-user-button";
 import { ListErrors } from "./ui_lib/list-error";
 import { SettingsForm } from "./settings-form";
+import  Header  from './header';
+import HeaderLoggedInView from "./header-logged-in-view";
+import HeaderLoggedOutView from "./header-logged-out-view";
 
 
 
@@ -17,8 +20,5 @@ import { SettingsForm } from "./settings-form";
 
 
 
-
-
-
-export { EditProfileSettings, FollowUserButton, Profile, ListErrors, SettingsForm }
+export { EditProfileSettings, FollowUserButton, Profile, ListErrors, SettingsForm, Header, HeaderLoggedInView, HeaderLoggedOutView }
 
