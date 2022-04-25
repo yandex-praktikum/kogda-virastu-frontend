a3e39c9e3d6088f469efaaba6b89d08aa0454d41
George Trubachev <trubacheff_91@mail.ru>
2022-04-25 18:07:13 +0300

    feat: рефакт deleteButton (#14)

---
+++ src/components_refact/ui_lib/delete-button/DeleteButton.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from "react";
+import { useDispatch } from "react-redux";
+import { DELETE_COMMENT } from "../../../constants";
+import {deleteComment} from '../../../services/api'
+
+export const DeleteButton: FC<{ slug: string, commentId: string, show: boolean }> = ({ slug, commentId, show }) => {
+    const dispatch = useDispatch()
+
+  const del = () => {
+        const payload = deleteComment(slug, commentId);
+        dispatch({ type: DELETE_COMMENT, payload, commentId })
+      }; 
+    
+        
+  if (show) {
+    return (
+      <span className="mod-options">
+        <i className="ion-trash-a" onClick={del}></i>
+      </span>
+    );
+  }
+  return null;
+
+    
+}
