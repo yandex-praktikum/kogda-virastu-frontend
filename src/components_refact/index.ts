92b7f6e0f0f4b0bfaa608651561c3d02b4fdf245
grecha1337 <4rmor3d@gmail.com>
2022-04-26 22:40:09 +0300

    Рефактор компонента comment-input (#23)

--- src/components_refact/index.ts
+++ src/components_refact/index.ts
@@ -2,6 +2,7 @@ import { Profile } from "./profile/profile";
 import { EditProfileSettings } from "./ui_lib/edit-profile-settings";
 import { FollowUserButton } from "./ui_lib/follow-user-button";
 import { ListErrors } from "./ui_lib/list-error";
+import CommentInput from "./comment-input";
 import { SettingsForm } from "./settings-form";
 import { Editor } from "./editor/Editor";
 import { ArticlePreview } from "./articleprevview/articlepreview";
@@ -11,26 +12,26 @@ import Banner from "./banner";
 import TagFilterTab from "./tag-filter-tab";
 import Comment from "./comment";
 import { Tags } from "./tags/Tags";
-import  Header  from './header';
+import Header from "./header";
 import HeaderLoggedInView from "./header-logged-in-view";
 import HeaderLoggedOutView from "./header-logged-out-view";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export { EditProfileSettings,TagFilterTab,Comment, Editor,Banner,Tags, FollowUserButton, Profile, ListErrors, SettingsForm, Header, HeaderLoggedInView, HeaderLoggedOutView, ArticleList, ListPagination, ArticlePreview }
-
-
+export {
+  EditProfileSettings,
+  TagFilterTab,
+  Comment,
+  Editor,
+  Banner,
+  Tags,
+  FollowUserButton,
+  Profile,
+  ListErrors,
+  SettingsForm,
+  Header,
+  HeaderLoggedInView,
+  HeaderLoggedOutView,
+  ArticleList,
+  ListPagination,
+  ArticlePreview,
+  CommentInput,
+};
