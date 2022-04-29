97b39ac643530cadf3d2b900b9a6740e44eeffc5
Elena Sharipova <elena.sh.r@yandex.ru>
2022-04-30 00:03:05 +0300

    refactor: banner with store (#46)

---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { useSelector } from '../../services/hooks';
+
+const Banner : FC = () => {
+  const { appName } = useSelector((state) => state.system);
+  const { isLoggedIn } = useSelector((state) => state.system);
+
+  return (
+    isLoggedIn
+      ? null
+      : (
+        <div className='banner'>
+          <div className='container'>
+            <h1 className='logo-font'>
+              {appName.toLowerCase()}
+            </h1>
+            <p>Your community project starter pack.</p>
+          </div>
+        </div>
+      )
+  );
+};
+
+export default Banner;
