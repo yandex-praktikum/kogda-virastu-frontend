a5b407f7075f5cb77ea6b7db6948576ca424b537
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-30 10:58:10 +0300

    [Refactor] Приведение patchUserThunk к единому стилю. (#49)
    Удаление неиспользуемого и устранение ошибок ESLint в хранилище.

--- src/thunks/patch-current-user-thunk.ts
+++ src/thunks/patch-current-user-thunk.ts
@@ -1,23 +1,45 @@
+import { batch } from 'react-redux';
+import { AxiosError } from 'axios';
 import { patchCurrentUser } from '../services/api';
 import { settingsPatchFailed, settingsPatchRequested, settingsPatchSucceeded } from '../store/apiSlice';
 import { resetFormProfile } from '../store/profileFormSubSlice';
-import { AppDispatch, AppThunk } from '../store/store.types';
+import { AppDispatch, AppThunk, RootState } from '../store/store.types';
+import makeErrorMessage from '../services/helpers/make-error-message';
+import { setUser } from '../store';
+import { TAPIError } from '../services/api.types';
 
-const patchCurrentUserThunk: AppThunk = (data: {
-  email: string;
-  username: string;
-  bio: string;
-  image: string;
-  password?: string;
-}) => async (dispatch: AppDispatch) => {
+const patchCurrentUserThunk: AppThunk = () => async (
+  dispatch: AppDispatch,
+  getState: () => RootState,
+) => {
+  dispatch(settingsPatchRequested());
+  const profile = getState().forms.profile ?? {};
+  // Type Guards
+  const user = profile.username || '';
+  const mail = profile.email || '';
+  const pass = profile.password || '';
+  const about = profile.bio || '';
+  const link = profile.image || '';
+
   try {
-    dispatch(settingsPatchRequested());
-    await patchCurrentUser(data);
-    dispatch(resetFormProfile());
-    dispatch(settingsPatchSucceeded());
+    const {
+      data: {
+        user: {
+          username, email, bio, image,
+        },
+      },
+    } = await patchCurrentUser({
+      username: user, email: mail, password: pass, bio: about, image: link,
+    });
+    batch(() => {
+      dispatch(setUser({
+        username, email, bio, image,
+      }));
+      dispatch(resetFormProfile());
+      dispatch(settingsPatchSucceeded());
+    });
   } catch (error) {
-    console.dir(error);
-    dispatch(settingsPatchFailed(error));
+    dispatch(settingsPatchFailed(makeErrorMessage(error as AxiosError<TAPIError>)));
   }
 };
 
