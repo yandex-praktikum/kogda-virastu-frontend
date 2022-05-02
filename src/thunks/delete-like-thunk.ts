8354cce6447a989f9d877b3d2f0ea9ec12a953b1
Evgeny Karpel <kspshnik@yandex.ru>
2022-05-02 12:14:36 +0300

    [Refactor] deleteLikeThunk() адаптирован под errorObject и переведен на view.feed вместо all.articles в свете пагинации.

--- src/thunks/delete-like-thunk.ts
+++ src/thunks/delete-like-thunk.ts
@@ -4,10 +4,10 @@ import {
   likeArticleDeleteFailed,
   likeArticleDeleteRequested,
   likeArticleDeleteSucceeded,
-  setAllArticles,
+  setViewFeed,
 } from '../store';
 import { deleteLikeArticle } from '../services/api';
-import makeErrorMessage from '../services/helpers/make-error-message';
+import { makeErrorObject } from '../services/helpers';
 import { TAPIError } from '../services/api.types';
 
 const deleteLikeThunk: AppThunk = (slug: string) => async (
@@ -18,11 +18,11 @@ const deleteLikeThunk: AppThunk = (slug: string) => async (
     dispatch(likeArticleDeleteRequested());
     const { data: { article } } = await deleteLikeArticle(slug);
     // Type Guard - в TAllState допускается null,  в TArticles - нет
-    const articles = getState().all.articles ?? [];
-    dispatch(setAllArticles(articles?.filter((item) => (item.slug !== article.slug))));
+    const articles = getState().view.feed ?? [];
+    dispatch(setViewFeed(articles?.filter((item) => (item.slug !== article.slug))));
     dispatch(likeArticleDeleteSucceeded());
   } catch (error) {
-    dispatch(likeArticleDeleteFailed(makeErrorMessage(error as AxiosError<TAPIError>)));
+    dispatch(likeArticleDeleteFailed(makeErrorObject(error as AxiosError<TAPIError>)));
   }
 };
 
