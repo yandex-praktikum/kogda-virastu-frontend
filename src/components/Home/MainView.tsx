e1eb6944f262fa9e49b82aa8f372861d5197032c
Evgeny Karpel <kspshnik@yandex.ru>
2022-05-03 17:54:37 +0300

    [Cleanup] Удаление неиспользуемого кода вынесенных компонент.

--- src/components/Home/MainView.tsx
+++ src/components/Home/MainView.tsx
@@ -8,75 +8,12 @@ import TagFilterTab from './TagFilterTab';
 import { FeedTypes } from '../../types/types';
 import { calculateOffset } from '../../services/helpers';
 
-
-
-const YourFeedTab = () => {
-    const dispatch = useDispatch()
-    const { isLoggedIn } = useSelector((state) => state.system);
-    const { positionFeed } = useSelector((state) => state.view)
-
-    const clickHandler = (e: any) => {
-        dispatch(loadPrivateFeedThunk())
-        dispatch(changePositionFeed('privat'))
-        e.preventDefault()
-
-    }
-    if (isLoggedIn) {
-        return (
-            <li className="nav-item">
-                <a href=""
-                    className={positionFeed === 'privat' ? 'nav-link active' : 'nav-link'}
-                    onClick={clickHandler}>
-                    Your Feed
-                </a>
-            </li>
-        )
-    }
-    else return null
-}
-const GlobalFeedTab = () => {
-    const dispatch = useDispatch();
-    const { positionFeed } = useSelector((state) => state.view)
-    const clickHandler = (e: any) => {
-        e.preventDefault();
-        dispatch(getAllTagsThunk())
-        dispatch(changePositionFeed('global'))
-    }
-    return (
-        <li className="nav-item">
-            <a
-                href=""
-                className={positionFeed === 'global' ? 'nav-link active' : 'nav-link'}
-                onClick={clickHandler}>
-                Global Feed
-            </a>
-        </li>
-    )
-}
-const TagFilterTab = (props: any) => {
-
-    if (!props.tag) {
-        return null;
-    }
-    else {
-        return (
-            <li className="nav-item">
-                <a href="" className="nav-link active">
-                    <i className="ion-pound"></i>
-                </a>
-            </li>
-
-        )
-    }
-}
-
-
-
-        const MainView = () => {
+const MainView = () => {
   const dispatch = useDispatch();
   const {
     page, perPage, feedType, tag,
   } = useSelector((state) => state.view);
+
   React.useEffect(() => {
     switch (feedType) {
       case FeedTypes.public: {
