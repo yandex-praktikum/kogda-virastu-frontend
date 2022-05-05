0eeb044e59110c492e596db218e2dd0b718eb242
Elena Sharipova <elena.sh.r@yandex.ru>
2022-05-05 13:30:20 +0300

    fix: excess fonts deleted

--- src/ui-lib/globalFontsStyled.ts
+++ src/ui-lib/globalFontsStyled.ts
@@ -16,28 +16,6 @@ const GlobalFontsStyles = `
     --default-h5-mobile: 400 18px/22px 'Alegreya Sans';
 
     --default-text-sans-med-18: 500 18px/24px 'Alegreya Sans';
-
-    --header-36: 600 36px/68px OpenSans;
-    --header-24: 600 24px/33px OpenSans;
-    --header-18: 600 18px/30px OpenSans;
-    --header-16: 600 16px/22px OpenSans;
-    --header-14: 600 14px/19px OpenSans;
-    --header-12: 600 12px/16px OpenSans;
-    --header-10: 600 10px/14px OpenSans;
-
-    --main-18: 400 18px/30px OpenSans;
-    --main-18-26-light: 300 18px/26px OpenSans;
-    --main-16: 400 16px/22px OpenSans;
-    --main-14-20: 400 14px/20px OpenSans;
-    --main-14-20-light: 300 14px/20px OpenSans;
-    --main-14-19: 400 14px/19px OpenSans;
-    --main-12: 400 12px/16px OpenSans;
-    --main-12-20: 400 12px/20px OpenSans;
-    --main-10: 400 10px/14px OpenSans;
-
-    --accent-14: italic 400 14px/19px OpenSans;
-    --accent-12: italic 400 12px/17px OpenSans;
-    --accent-10: italic 400 10px/14px OpenSans;
   }
 `;
 
