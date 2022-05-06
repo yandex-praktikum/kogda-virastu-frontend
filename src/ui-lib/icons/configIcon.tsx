ce1fae229730a84d47b49699b669537b6ec13120
Lex <Lexor.V.A.S@yandex.ru>
2022-05-07 02:52:50 +0300

    add icons constructor

---
+++ src/ui-lib/icons/configIcon.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+import { FC, useState } from 'react';
+
+type TcontainerIconStyle =  {
+    width?:string;
+    height?: string;
+    isHover?:boolean;
+    cursor?: 'default' | 'pointer' | 'none';
+}
+
+const ContainerIcon = styled.div<TcontainerIconStyle>`
+width: ${({width = '24px'}) => width};
+height:${({height = '24px'}) => height};
+cursor: ${({cursor = 'pointer'}) => cursor };
+${({isHover}) => isHover ? `:hover {opacity:0.5}`: '' }
+`
+type TConstuctorIcon = {
+    onClick?(e: React.MouseEvent<HTMLDivElement>): void;
+    onBlur?(e?: React.FocusEvent<HTMLDivElement>): void;
+    picture:any
+    width?:string;
+    height?: string;
+    isHover?: boolean;
+    cursor?: 'default' | 'pointer' | 'none';
+}
+
+
+export const ConstuctorIcon:FC<TConstuctorIcon> = ({picture, onClick, width, height, isHover, onBlur, cursor}) => {
+
+    return(
+        <>
+        <ContainerIcon onClick={onClick} width = {width} height={height} isHover={isHover} onBlur={onBlur} cursor={cursor} >
+        <img src={picture} />
+        </ContainerIcon>
+        </>
+    )
+}
---
+++ src/ui-lib/icons/icons.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import heartFull from '../../imges/icons/heartFull.png';
+import {ConstuctorIcon} from './configIcon';
+
+type Ticons = {
+    onClick?(e: React.MouseEvent<HTMLDivElement>): void;
+    onBlur?(e?: React.FocusEvent<HTMLDivElement>): void;
+    width?:string;
+    height?: string;
+    isHover?: boolean;
+    cursor?: 'default' | 'pointer' | 'none';
+}
+export const HeartsFullIcon:FC<Ticons> = ({onClick, width, height, isHover, onBlur, cursor }) => {
+    return (
+        <>
+        <ConstuctorIcon picture={heartFull}  onClick={onClick} width = {width} height={height} isHover={isHover} onBlur={onBlur} cursor={cursor} />
+        </>
+    )
+}
