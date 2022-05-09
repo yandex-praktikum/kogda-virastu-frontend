08ffb3fc94c069152c164d449f01a9ad434caf16
Evgeny Karpel <kspshnik@yandex.ru>
2022-05-09 15:30:53 +0300

    [Refactor] Новые кнопки частично

--- src/ui-lib/icons.tsx
+++ src/ui-lib/icons.tsx
@@ -1,134 +1,111 @@
-import DeletePic from '../assets/images/icons/trash-icon.svg';
-// import EditPic from '../assets/images/icons/edit-icon.svg';
+import styled from 'styled-components';
+import React, { FC } from 'react';
 import { ReactComponent as EditPic } from '../assets/images/icons/edit-icon.svg';
-import {TColorSet} from "./styles.types";
-import styled from "styled-components";
-import React, {FC, MouseEventHandler} from "react";
-const whiteIconStyle: TColorSet = {
-  defaultColor: '#FFFFFF',
-  hoverColor: '#FFFFFF',
-  activeColor: '#FFFFFF',
-  disabledColor: '#CCCCCC',
-};
+import { ReactComponent as BasketPic } from '../assets/images/icons/trash-icon.svg';
+import { ReactComponent as defaultAvatar } from '../assets/images/icons/avatar-icon.svg';
+import { TAvatarSizes, TAvatarIconProps } from '../types/styles.types';
 
-const redIconStyle: TColorSet = {
-  defaultColor: '#FF413B',
-  hoverColor: '#E53B35',
-  activeColor: '#CC342F',
-  disabledColor: '#CCCCCC',
-};
-
-const blueIconStyle: TColorSet = {
-  defaultColor: '#008AFF',
-  hoverColor: '#007CE5',
-  activeColor: '#006ECC',
-  disabledColor: '#CCCCCC',
-};
-
-const getColor = (disabled : boolean, baseColor : string, disabledColor: string) : string => {
-  if (disabled) {
-    return disabledColor;
-  }
-  return baseColor;
-};
-
-type TBasicIconProps = {
-  defaultColor: string;
-  hoverColor: string;
-  activeColor: string;
-};
-const basicIconStyles = `
-  width: 24px;
-  height: 24px;
-  display: block;
-`;
-
-const BasicEditIcon = styled<TBasicIconProps>(EditPic)`
-  width: 24px;
-  height: 24px;
-  display: block;
-  & > svg > path {
-    stroke: ${({ defaultColor }) => defaultColor};
-    }
-  &:hover > svg > path {
-    stroke: ${({ hoverColor }) => hoverColor};
-    }
-  &:focus > svg > path {
-    stroke: ${({ defaultColor }) => defaultColor};
-    }
-  &:active > svg > path {
-    stroke: ${({ activeColor }) => activeColor};
-    }
-`;
+import { getAvatarBorderProp } from '../services/helpers';
+import { blue } from '../constants/colors';
 
 type TIconProps = {
-  onClick?: MouseEventHandler<HTMLImageElement>;
-  disabled: boolean;
-  colors: TColorSet;
-};
-/* const DeleteIcon : FC<TIconProps> = ({ onClick = undefined, disabled, colors }) => {
-  const {
-    defaultColor,
-    activeColor,
-    hoverColor,
-    disabledColor,
-  } = colors;
-  const defColor = (disabledState : boolean) : string => {
-    if (disabledState) {
-      return disabledColor;
-    }
-    return defaultColor;
-  };
-  const hovColor = (disabledState : boolean) : string => {
-    if (disabledState) {
-      return disabledColor;
-    }
-    return hoverColor;
-  };
-  const actColor = (disabledState : boolean) : string => {
-    if (disabledState) {
-      return disabledColor;
-    }
-    return activeColor;
-  };
-  return (
-    <BasicIcon
-      src={DeletePic as string}
-      alt='Иконка удаления'
-      onClick={onClick}
-      defaultColor={defColor(disabled)}
-      activeColor={actColor(disabled)}
-      hoverColor={hovColor(disabled)} />
-  );
-};
-*/
-const EditIcon : FC<TIconProps> = ({ onClick = undefined, disabled = false, colors }) => {
-  const {
-    defaultColor,
-    hoverColor,
-    activeColor,
-    disabledColor,
-  } = colors;
-  return (
-    <BasicEditIcon
-      defaultColor={getColor(disabled, defaultColor, disabledColor)}
-      hoverColor={getColor(disabled, hoverColor, disabledColor)}
-      activeColor={getColor(disabled, activeColor, disabledColor)} />
-  );
+  color: string;
+  distance?: number;
 };
 
-/* const EditIconRaw = styled<TRawIconProps>(EditPic)`
-  ${basicIconStyles}
+type TAvatarSize = {
+  [size in TAvatarSizes]: {
+    width: number;
+    height: number;
+  }
+};
+
+const avatarSize : TAvatarSize = {
+  large: {
+    width: 320,
+    height: 320,
+  },
+  small: {
+    width: 24,
+    height: 24,
+  },
+};
+
+type TBorderProps = {
+  width: number;
+  color: string;
+};
+
+interface IBasicAvatar {
+  bordered: boolean,
+  borderProps: TBorderProps,
+  distance?: number,
+  color?: string,
+}
+
+export const EditIcon = styled(EditPic)<TIconProps>`
+  width: 24px;
+  height: 24px;
+  display: block;
+  margin-right: ${({ distance }) => distance ?? 0}px;
   & > path {
-    stroke: ${({ colors: { defaultColor } }) => defaultColor};
+    stroke: ${({ color }) => color};
     }
-  &:hover > path {
-    stroke: ${({ colors: { hoverColor } }) => hoverColor};
+`;
+export const DeleteIcon = styled(BasketPic)<TIconProps>`
+  width: 24px;
+  height: 24px;
+  display: block;
+  margin-right: ${({ distance }) => distance ?? 0}px;
+  & > path {
+    stroke: ${({ color }) => color};
     }
-  &:focus > path {
-    stroke: ${({ colors: { defaultColor } }) => defaultColor};
+`;
+const DefaultAvatar = styled(defaultAvatar)<TIconProps>`
+  display: block;
+  margin-right: ${({ distance }) => distance ?? 0}px;
+  & > path {
+    stroke: ${({ color }) => color};
     }
-  &:active > path {
-    stroke: ${({ colors: { activeColor } }) => activeColor};
-    }
-`; */
+`;
+
+const BasicAvatar = styled.img<IBasicAvatar>`
+  box-sizing: border-box;
+  border-radius: 50%;
+  margin-right: ${({ distance }) => distance ?? 0}px;
+  border-width: ${({ bordered, borderProps: { width } }) => getAvatarBorderProp(bordered, width)}px;
+  border-color: ${({ bordered, borderProps: { color } }) => getAvatarBorderProp(bordered, color)};
+`;
+
+export const AvatarIcon : FC<TAvatarIconProps> = ({
+  size,
+  name,
+  image,
+  distance,
+  color,
+}) => {
+  const borderProps : TBorderProps = {
+    width: 2,
+    color: blue,
+  };
+
+  if (!image) {
+    return (
+      <DefaultAvatar
+        width={`${avatarSize[size].width}px`}
+        height={`${avatarSize[size].height}px`}
+        distance={distance}
+        color={color} />
+    );
+  }
+  return (
+    <BasicAvatar
+      borderProps={borderProps}
+      bordered={!!image && size === 'small'}
+      alt={name}
+      src={image}
+      width={`${avatarSize[size].width}px`}
+      height={`${avatarSize[size].height}px`}
+      distance={distance} />
+  );
+};
