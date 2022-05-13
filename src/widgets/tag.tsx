e69679482841778b5348b2577a717a971e116d07
Ilya Pichugin <ilia-pichugin2014@yandex.ru>
2022-05-13 09:17:25 +0300

    Исправлены ошибки в PopularTags

--- src/widgets/tag.tsx
+++ src/widgets/tag.tsx
@@ -2,28 +2,20 @@ import React, { FC, MouseEventHandler, MouseEvent } from 'react';
 import styled, { useTheme } from 'styled-components';
 import { CrossIcon } from '../ui-lib';
 
-interface ITagProps {
-  tag: string,
-  handleClick?(e: React.MouseEvent<HTMLButtonElement>, tag: string): void,
-  isActive: boolean,
-  inactiveTag?: MouseEventHandler<SVGSVGElement>,
+type TButtonProps = {
+  isActive: boolean;
 }
 
-const Tag: FC<ITagProps> = ({
-  tag, handleClick, isActive, inactiveTag,
-}) => {
-  const theme = useTheme();
-
-  const Button = styled.button`
+const Button = styled.button<TButtonProps>`
     padding: 0;
     border: none;
-    font-family: ${theme.text18Sans.family};
-    font-weight: ${theme.text18Sans.weight};
-    font-size: ${theme.text18Sans.size}px;
-    line-height: ${theme.text18Sans.height}px;
+    font-family: ${({ theme }) => theme.text18Sans.family};
+    font-weight: ${({ theme }) => theme.text18Sans.weight};
+    font-size: ${({ theme }) => theme.text18Sans.size}px;
+    line-height: ${({ theme }) => theme.text18Sans.height}px;
     display: flex;
     align-items: center;
-    color: ${isActive ? theme.button.blue.default : theme.secondaryText};
+    color: ${({ isActive, theme }) => isActive ? theme.button.blue.default : theme.secondaryText};
     background-color: transparent;
 
     :active {
@@ -31,15 +23,28 @@ const Tag: FC<ITagProps> = ({
     }
   `;
 
+interface ITagProps {
+  tag: string,
+  handleClick?(e: React.MouseEvent<HTMLButtonElement>, tag: string): void,
+  isActive: boolean,
+  deactivationTag?: MouseEventHandler<SVGSVGElement>,
+}
+
+const Tag: FC<ITagProps> = ({
+  tag, handleClick, isActive, deactivationTag,
+}) => {
+  const theme = useTheme();
+
   return (
     <Button
+      isActive = {isActive}
       type='button'
       key={tag}
       onClick={handleClick && ((e) => handleClick(e, tag))}>
       #
       {tag}
       {' '}
-      {isActive && <CrossIcon color='blue' onClick={inactiveTag} />}
+      {isActive && deactivationTag && <CrossIcon color={theme.markedText} onClick={deactivationTag} />}
     </Button>
   );
 };
