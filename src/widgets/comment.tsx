ea31f99afdd10dd937705745f35494a535b1c8d1
grecha1337 <4rmor3d@gmail.com>
2022-05-13 19:52:41 +0300

    Потерянный комментарий

--- src/widgets/comment.tsx
+++ src/widgets/comment.tsx
@@ -1,18 +1,54 @@
 import styled from 'styled-components';
 import React, { FC } from 'react';
+import CommentAuthorHeading from './comment-author-heading';
 
 const CommentContainer = styled.div`
   width: 100%;
   display: flex;
-  background:${({ theme: { bgPrimary } }) => bgPrimary};
-  border-radius: 4px 4px 0px 0px;
+  background: ${({ theme: { bgPrimary } }) => bgPrimary};
+  border-radius: 4px;
+  border: 1px solid ${({ theme: { dividerColor } }) => dividerColor};
   padding: 24px 24px 16px;
   flex-flow: column nowrap;
   justify-content: space-between;
   gap: 24px 0;
 `;
 
-const Comment: FC = ({ }) => (
+type TCommentProps = {
+  image?: string;
+  createAt: Date;
+  name: string;
+  onDeleteClick: React.MouseEventHandler<SVGSVGElement>;
+  isAuthor: boolean,
+  body: string,
+};
+
+const CommentBody = styled.p`
+  color: ${({ theme }) => theme.primaryText};
+  font-size: ${({ theme }) => theme.text16.size}px;
+  font-family: ${({ theme }) => theme.text16.family};
+  font-weight: ${({ theme }) => theme.text16.weight};
+  line-height: ${({ theme }) => theme.text16.height}px;
+  margin: 0;
+`;
+
+const Comment: FC<TCommentProps> = ({
+  image,
+  createAt,
+  name,
+  isAuthor,
+  onDeleteClick,
+  body,
+}) => (
   <CommentContainer>
+    <CommentAuthorHeading
+      image={image}
+      date={createAt}
+      name={name}
+      onDeleteClick={onDeleteClick}
+      isAuthor={isAuthor} />
+    <CommentBody>{body}</CommentBody>
   </CommentContainer>
-)
+);
+
+export default Comment;
