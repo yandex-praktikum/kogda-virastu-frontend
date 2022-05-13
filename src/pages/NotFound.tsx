d4b3df666055676b29da07d3ac5f501557a385bd
Lex <Lexor.V.A.S@yandex.ru>
2022-05-13 21:41:13 +0300

    add mini screen style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -10,6 +10,10 @@ line-height: 118px;
 font-weight: ${({ theme: { secondLevelHeading: { weight } } }) => weight};
 text-align: center;
 margin-bottom: 24px;
+@media screen and (max-width:320px) {
+    font-size: 86px;
+    line-height: 86px;
+}
 `
 const NotFoundStyle = styled.section`
     box-sizing: border-box;
@@ -18,6 +22,11 @@ const NotFoundStyle = styled.section`
     flex-direction: column;
     align-items: center;
     min-height:747px;
+    @media screen and (max-width:320px) {
+        padding-top:40px;
+        min-height:307px;
+
+}
 `
 const LinkStyle = styled(Link)`
     color: ${((props) => props.theme.markedText)};
@@ -30,6 +39,14 @@ font-family: ${({ theme: { text18Sans: { family } } }) => family};
   font-size: ${({ theme: { text18Sans: { size } } }) => size}px;
   font-weight: ${({ theme: { text18Sans: { weight } } }) => weight};
   line-height: ${({ theme: { text18Sans: { height } } }) => height}px;
+  @media screen and (max-width:320px) {
+    max-width: 213px;
+    font-family: ${({ theme: { text16Sans: { family } } }) => family};
+  font-size: ${({ theme: { text16Sans: { size } } }) => size}px;
+  font-weight: ${({ theme: { text16Sans: { weight } } }) => weight};
+  line-height: ${({ theme: { text16Sans: { height } } }) => height}px;
+
+  }
 `
 
 const  NotFound = () => {
