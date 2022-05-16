40254fc2036446876c433484d0b2dbcf5ec55fea
Evgeny Karpel <kspshnik@yandex.ru>
2022-05-16 17:28:58 +0300

    [New] Метки для textarea в словарик - 2.

--- src/vocabularies/ru.ts
+++ src/vocabularies/ru.ts
@@ -38,6 +38,7 @@ const ru : TVocabulary = {
   publishComment: 'Опубликовать комментарий',
   loading: 'Подождите, данные загружаются...',
   footerMessage: 'Сделано студентами Яндекс Практикума. Команда Red.Types Lakers',
+  aboutUser: 'Расскажите о себе',
 };
 
 export default ru;
