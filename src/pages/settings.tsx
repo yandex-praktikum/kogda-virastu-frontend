680794f1efaedba223110335be1cce0daa10ed2a
grecha1337 <4rmor3d@gmail.com>
2022-05-17 00:13:27 +0300

    Поправил return  в settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -8,12 +8,10 @@ const Page = styled.section`
   justify-content: center;
 `;
 
-
-const Settings = () => {
+const Settings = () => (
   <Page>
     <SettingsForm />
   </Page>
-};
-
+);
 
 export default Settings;
