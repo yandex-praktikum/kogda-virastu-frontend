6efb811c5d03452cb39ca506e6d54d1dac2a9591
Aleksey <1862647@gmail.com>
2022-06-05 12:18:52 +0700

    feat: split posts on 2 columns

--- src/widgets/feed-ribbon.tsx
+++ src/widgets/feed-ribbon.tsx
@@ -1,44 +1,72 @@
-import React, { FC, MouseEventHandler } from 'react';
+import React, { FC, MouseEventHandler, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from '../services/hooks';
-import { RegularText } from '../ui-lib';
+import { Divider, RegularText } from '../ui-lib';
 import ScrollRibbon from './scroll-ribbon';
 import ArticleFullPreview from './article-full-preview';
 import { addLikeThunk, deleteLikeThunk } from '../thunks';
 
 const RibbonWrapper = styled.ul`
-width: 100%;
+  box-sizing: border-box;
+  width: 100%;
   height: 100%;
   display: flex;
-  flex-flow: column nowrap;
-  justify-content: flex-start;
-  align-items: center;
+  flex-flow: row wrap;
+  justify-content: space-between;
   list-style: none outside;
   margin: 0;
-  padding: 0;
   margin-block-start: 0;
   margin-block-end: 0;
   padding-inline-start: 0;
   padding-inline-end: 0;
+  padding-left: 5%;
   gap: 32px;
+
+  @media screen and (max-width: 800px) {
+    column-gap: 20px;
+    row-gap: 32px;
+}
+
+@media screen and (max-width: 765px) {
+  flex-flow: column nowrap;
+  padding: 0;
+}
 `;
 
 const ItemWrapper = styled.li`
   list-style: none outside;
+  max-width: calc(50% - 32px);
+  width: 100%;
   margin: 0;
   padding: 0;
   margin-block-start: 0;
   margin-block-end: 0;
   padding-inline-start: 0;
   padding-inline-end: 0;
+
+  @media screen and (max-width: 800px) {
+    max-width: calc(50% - 20px);
+}
+
+@media screen and (max-width: 765px) {
+  max-width: max-content;
+}
 `;
 
 const FeedRibbon : FC = () => {
+  const [mobileScreen, setMobileScreen] = useState(false);
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.view.feed);
   const tags = useSelector((state) => state.view.selectedTags) ?? [];
   const { isPublicFeedFetching } = useSelector((state) => state.api);
+  window.addEventListener('resize', () => {
+    if (window.screen.width > 765) {
+      setMobileScreen(true);
+    } else {
+      setMobileScreen(false);
+    }
+  });
   if (!posts || isPublicFeedFetching) {
     return (
       <RegularText size='large' weight={500}>
@@ -51,7 +79,7 @@ const FeedRibbon : FC = () => {
       <RibbonWrapper>
         {posts.filter((post) => post.tagList.some((tag) => (tags.includes(tag)
             || !tags
-            || tags.length < 1))).map((post) => {
+            || tags.length < 1))).map((post, i) => {
           const onClick : MouseEventHandler = () => {
             if (post.favorited) {
               dispatch(deleteLikeThunk(post.slug));
@@ -60,11 +88,15 @@ const FeedRibbon : FC = () => {
             }
           };
           return (
-            <ItemWrapper key={post.slug}>
-              <ArticleFullPreview
-                article={post}
-                onLikeClick={onClick} />
-            </ItemWrapper>
+            <>
+              <ItemWrapper key={post.slug}>
+                <ArticleFullPreview
+                  article={post}
+                  onLikeClick={onClick} />
+              </ItemWrapper>
+              {i % 2 && i !== posts.length - 1 && mobileScreen
+                ? <Divider distance={0} /> : null}
+            </>
           );
         })}
       </RibbonWrapper>
