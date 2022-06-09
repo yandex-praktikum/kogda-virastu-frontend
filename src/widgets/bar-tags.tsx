3ab99c75d75c885a96dfa8c2f3473d21a3f9b4bb
Kettrin62 <Kettykazakova@yandex.ru>
2022-06-09 14:02:15 +0300

    feat: всплывающее окно

--- src/widgets/bar-tags.tsx
+++ src/widgets/bar-tags.tsx
@@ -1,11 +1,12 @@
-import React, { FC, MouseEvent } from 'react';
-import styled from 'styled-components';
+import React, { FC, MouseEvent, useState } from 'react';
+import styled, { useTheme } from 'styled-components';
 import { nanoid } from '@reduxjs/toolkit';
 import { useSelector, useDispatch } from '../services/hooks';
 import { setTagsFollow } from '../store';
 import Tag from './tag';
 import addTagFollowThunk from '../thunks/add-tag-follow-thunk';
 import deleteTagFollowThunk from '../thunks/delete-tag-follow-thunk';
+import { RegularText } from '../ui-lib';
 
 type TBarTags = {
   tagList: string[],
@@ -21,6 +22,7 @@ const Lists = styled.ul<TLists>`
     display: flex;
     box-sizing:border-box;
     flex-wrap:wrap;
+    position: relative;
     /*
     flex-direction: ${({ rowReverse }) => rowReverse && 'row-reverse'};
     */
@@ -51,16 +53,35 @@ const List = styled.li`
     list-style-type: none;
 `;
 
+const MessageContainer = styled.div`
+  padding: 0 16px;
+  height: 32px;
+  background-color: rgba(10, 10, 11, 0.9);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 32px;
+  position: absolute;
+  top: -56px;
+  left: calc(50% - 267px / 2);
+`;
+
 const BarTags: FC<TBarTags & TLists> = ({ tagList, isHasImage = false, rowReverse = false }) => {
   const { tagsFollow } = useSelector((state) => state.view);
   const dispatch = useDispatch();
   const pointer = !rowReverse;
+  const theme = useTheme();
+  const [visible, setVisible] = useState(false);
+  const [tagName, setTagName] = useState('');
 
   const handleClickTag = (e: MouseEvent<HTMLButtonElement>, tag: string, isActive: boolean) => {
     e.preventDefault();
     if (pointer) {
       if (!isActive) {
         dispatch(addTagFollowThunk(tag));
+        setVisible(true);
+        setTagName(tag);
+        setTimeout(() => setVisible(false), 2000);
         if (tagsFollow) {
           dispatch(setTagsFollow([...tagsFollow, tag]));
         } else {
@@ -84,6 +105,18 @@ const BarTags: FC<TBarTags & TLists> = ({ tagList, isHasImage = false, rowRevers
             isLocationArticle={pointer}
             isActive={!!tagsFollow?.includes(tag)}
             handleClick={handleClickTag} />
+          {visible && (
+            <MessageContainer>
+              <RegularText
+                size='medium'
+                weight={500}
+                color={theme.button.blue.font}
+                sansSerif>
+                Вы подписались на тег #
+                {tagName}
+              </RegularText>
+            </MessageContainer>
+          )}
         </List>
       ))}
     </Lists>
